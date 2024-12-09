05079418125efcb47ad7f68d0dad08512f78dab1
Abdullah Haris <abdullaharis111@gmail.com>
2024-12-09 07:29:17 +0530

    added more dummy products

--- lib/modern_ecommerce/constants/dummy_data.dart
+++ lib/modern_ecommerce/constants/dummy_data.dart
@@ -29,10 +29,29 @@ final List<Category> dummyCategories = [
     imageUrl: 'https://images.pexels.com/photos/3373736/pexels-photo-3373736.jpeg?auto=compress&cs=tinysrgb&w=300',
     svgIcon: MESvgIcons.beauty,
   ),
+  Category(
+    id: '5',
+    name: 'Sports',
+    imageUrl: 'https://images.pexels.com/photos/4761352/pexels-photo-4761352.jpeg?auto=compress&cs=tinysrgb&w=300',
+    svgIcon: MESvgIcons.sports,
+  ),
+  Category(
+    id: '6',
+    name: 'Books',
+    imageUrl: 'https://images.pexels.com/photos/2908984/pexels-photo-2908984.jpeg?auto=compress&cs=tinysrgb&w=300',
+    svgIcon: MESvgIcons.books,
+  ),
+  Category(
+    id: '7',
+    name: 'Toys',
+    imageUrl: 'https://images.pexels.com/photos/163696/toy-car-toy-box-mini-163696.jpeg?auto=compress&cs=tinysrgb&w=300',
+    svgIcon: MESvgIcons.toys,
+  ),
 ];
 
 /// Dummy Products
 final List<Product> dummyProducts = [
+  // Electronics
   Product(
     id: '1',
     name: 'Sony WH-1000XM4',
@@ -55,6 +74,18 @@ final List<Product> dummyProducts = [
   ),
   Product(
     id: '3',
+    name: 'MacBook Pro 14"',
+    description: 'M1 Pro chip, 16GB RAM, 512GB SSD',
+    price: 1999.99,
+    imageUrl: 'https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?auto=compress&cs=tinysrgb&w=400',
+    rating: 4.9,
+    reviews: 438,
+    category: 'Electronics',
+  ),
+
+  // Fashion
+  Product(
+    id: '4',
     name: 'Nike Air Max 270',
     description: 'Men\'s Running Shoes, Black/White',
     price: 150.00,
@@ -64,7 +95,29 @@ final List<Product> dummyProducts = [
     category: 'Fashion',
   ),
   Product(
-    id: '4',
+    id: '5',
+    name: 'Leather Jacket',
+    description: 'Classic Motorcycle Jacket, Brown',
+    price: 199.99,
+    imageUrl: 'https://images.pexels.com/photos/1124468/pexels-photo-1124468.jpeg?auto=compress&cs=tinysrgb&w=400',
+    rating: 4.6,
+    reviews: 567,
+    category: 'Fashion',
+  ),
+  Product(
+    id: '6',
+    name: 'Ray-Ban Aviator',
+    description: 'Classic Sunglasses, Gold Frame',
+    price: 154.00,
+    imageUrl: 'https://images.pexels.com/photos/701877/pexels-photo-701877.jpeg?auto=compress&cs=tinysrgb&w=400',
+    rating: 4.7,
+    reviews: 1203,
+    category: 'Fashion',
+  ),
+
+  // Home
+  Product(
+    id: '7',
     name: 'Philips Hue Starter Kit',
     description: 'Smart LED Bulb Kit with Bridge, Multi-color',
     price: 199.99,
@@ -74,7 +127,29 @@ final List<Product> dummyProducts = [
     category: 'Home',
   ),
   Product(
-    id: '5',
+    id: '8',
+    name: 'Dyson V15 Detect',
+    description: 'Cordless Vacuum Cleaner',
+    price: 699.99,
+    imageUrl: 'https://images.pexels.com/photos/4108715/pexels-photo-4108715.jpeg?auto=compress&cs=tinysrgb&w=400',
+    rating: 4.8,
+    reviews: 325,
+    category: 'Home',
+  ),
+  Product(
+    id: '9',
+    name: 'Nespresso Vertuo',
+    description: 'Coffee and Espresso Machine',
+    price: 199.00,
+    imageUrl: 'https://images.pexels.com/photos/4349727/pexels-photo-4349727.jpeg?auto=compress&cs=tinysrgb&w=400',
+    rating: 4.7,
+    reviews: 892,
+    category: 'Home',
+  ),
+
+  // Beauty
+  Product(
+    id: '10',
     name: 'La Mer Moisturizer',
     description: 'Crème de la Mer Moisturizing Cream, 2 oz',
     price: 350.00,
@@ -82,6 +157,48 @@ final List<Product> dummyProducts = [
     rating: 4.9,
     reviews: 445,
     category: 'Beauty',
+  ),
+  Product(
+    id: '11',
+    name: 'Dyson Airwrap',
+    description: 'Complete Styler for Multiple Hair Types',
+    price: 599.99,
+    imageUrl: 'https://images.pexels.com/photos/3993449/pexels-photo-3993449.jpeg?auto=compress&cs=tinysrgb&w=400',
+    rating: 4.7,
+    reviews: 1567,
+    category: 'Beauty',
+  ),
+  Product(
+    id: '12',
+    name: 'SK-II Essence',
+    description: 'Facial Treatment Essence, 230ml',
+    price: 185.00,
+    imageUrl: 'https://images.pexels.com/photos/4465124/pexels-photo-4465124.jpeg?auto=compress&cs=tinysrgb&w=400',
+    rating: 4.8,
+    reviews: 892,
+    category: 'Beauty',
+  ),
+
+  // Sports
+  Product(
+    id: '13',
+    name: 'Peloton Bike+',
+    description: 'Smart Indoor Exercise Bike',
+    price: 2495.00,
+    imageUrl: 'https://images.pexels.com/photos/4162489/pexels-photo-4162489.jpeg?auto=compress&cs=tinysrgb&w=400',
+    rating: 4.9,
+    reviews: 756,
+    category: 'Sports',
+  ),
+  Product(
+    id: '14',
+    name: 'Tennis Racket Pro',
+    description: 'Wilson Pro Staff RF97 Autograph',
+    price: 259.00,
+    imageUrl: 'https://images.pexels.com/photos/209977/pexels-photo-209977.jpeg?auto=compress&cs=tinysrgb&w=400',
+    rating: 4.7,
+    reviews: 445,
+    category: 'Sports',
   ),
 ];
 
--- lib/modern_ecommerce/constants/svg_icons.dart
+++ lib/modern_ecommerce/constants/svg_icons.dart
@@ -2,25 +2,47 @@
 class MESvgIcons {
   static const String electronics = '''
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M12 16H12.01M7 19H17C18.1046 19 19 18.1046 19 17V7C19 5.89543 18.1046 5 17 5H7C5.89543 5 5 5.89543 5 7V17C5 18.1046 5.89543 19 7 19ZM7 19L17 19" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+  <path d="M21 16H3V4h18v12zm0-14H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7v2H8v2h8v-2h-2v-2h7c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z" fill="currentColor"/>
 </svg>
 ''';
 
   static const String fashion = '''
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M3.5 7C3.5 7 5.5 9 12 9C18.5 9 20.5 7 20.5 7M3.5 7C3.5 7 3 4 12 4C21 4 20.5 7 20.5 7M3.5 7L5 20H19L20.5 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M21.6 12l-3.3-6.6C18.1 4.9 17.6 4 16.5 4H7.5C6.4 4 5.9 4.9 5.7 5.4L2.4 12c-.3.6-.4 1.3-.4 2 0 2.2 1.8 4 4 4h12c2.2 0 4-1.8 4-4 0-.7-.1-1.4-.4-2zM12 17c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z" fill="currentColor"/>
 </svg>
 ''';
 
   static const String home = '''
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M3 12L5 10M5 10L12 3L19 10M5 10V20C5 20.5523 5.44772 21 6 21H9M19 10L21 12M19 10V20C19 20.5523 18.5523 21 18 21H15M9 21C9.55228 21 10 20.5523 10 20V16C10 15.4477 10.4477 15 11 15H13C13.5523 15 14 15.4477 14 16V20C14 20.5523 14.4477 21 15 21M9 21H15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M12 3L4 9v12h5v-7h6v7h5V9l-8-6z" fill="currentColor"/>
 </svg>
 ''';
 
   static const String beauty = '''
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M7 21L12 16M12 16L17 21M12 16V4M12 4H7M12 4H17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M20 12c0-1.1-.9-2-2-2V7c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v3c-1.1 0-2 .9-2 2v5h1.33L6 19h1l.67-2h8.67l.66 2h1l.67-2H20v-5zm-4-2H8V7h8v3z" fill="currentColor"/>
 </svg>
 ''';
-} 
+
+  static const String sports = '''
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.58 20 4 16.42 4 12C4 7.58 7.58 4 12 4C16.42 4 20 7.58 20 12C20 16.42 16.42 20 12 20Z" fill="currentColor"/>
+  <path d="M12 11L16 7L12 3L8 7L12 11ZM12 13L8 17L12 21L16 17L12 13Z" fill="currentColor"/>
+</svg>
+''';
+
+  static const String books = '''
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M4 6H2V20C2 21.1 2.9 22 4 22H18V20H4V6Z" fill="currentColor"/>
+  <path d="M20 2H8C6.9 2 6 2.9 6 4V16C6 17.1 6.9 18 8 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2ZM20 16H8V4H20V16Z" fill="currentColor"/>
+  <path d="M10 9H18V11H10V9ZM10 12H14V14H10V12ZM10 6H18V8H10V6Z" fill="currentColor"/>
+</svg>
+''';
+
+  static const String toys = '''
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12ZM12 6C13.1 6 14 6.9 14 8C14 9.1 13.1 10 12 10C10.9 10 10 9.1 10 8C10 6.9 10.9 6 12 6Z" fill="currentColor"/>
+  <path d="M18 14H6C4.9 14 4 14.9 4 16V18C4 19.1 4.9 20 6 20H18C19.1 20 20 19.1 20 18V16C20 14.9 19.1 14 18 14ZM18 18H6V16H18V18Z" fill="currentColor"/>
+</svg>
+''';
+}
